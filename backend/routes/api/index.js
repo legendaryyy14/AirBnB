dee8b9abf6a7ca3ce8449802af6129e7bb0e08b6
JamieCardona <jamie_cardona@yahoo.com>
2023-08-07 22:05:30 -0600

    idk

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,3 @@
-const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
@@ -6,6 +5,10 @@ const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
 const spotImagesRouter = require('./spotimages.js');
 const reviewImagesRouter = require('./reviewimages.js')
+
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -27,6 +30,39 @@ router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotImagesRouter);
 
 router.use('/review-images', reviewImagesRouter);
+
+router.use('/api', apiRouter);
+
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 
 router.post('/test', (req, res) => {
