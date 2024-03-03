c192ada35b9899c4a2a5d275fb63bb56ba226686
legendaryyy14 <jamiecardona96@gmail.com>
2024-03-02 18:13:33 -0700

    added seeders but getting migration errors

--- backend/db/seeders/6-spotimage.js
+++ backend/db/seeders/6-spotimage.js
@@ -65,6 +65,111 @@ module.exports = {
       preview: false,
       spotId: 3
     },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693083881/pexels-photo-106399_gg3pvm.jpg",
+      preview: true,
+      spotId: 4
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693083881/pexels-photo-1115804_mnlpkm.jpg",
+      preview: true,
+      spotId: 5
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693083881/pexels-photo-164338_ksdu9n.jpg",
+      preview: true,
+      spotId: 6
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084183/pexels-photo-2121121_s1oh3m.jpg",
+      preview: false,
+      spotId: 4
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-2635038_wj3xvr.webp",
+      preview: false,
+      spotId: 5
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-2724749_r7i2ng.webp",
+      preview: false,
+      spotId: 6
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084183/pexels-photo-1080696_zaryd5.jpg",
+      preview: false,
+      spotId: 4
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-276724_vyzstu.jpg",
+      preview: false,
+      spotId: 5
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1692757511/samples/balloons.jpg",
+      preview: false,
+      spotId: 6
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693083881/pexels-photo-106399_gg3pvm.jpg",
+      preview: true,
+      spotId: 1
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693083881/pexels-photo-1115804_mnlpkm.jpg",
+      preview: true,
+      spotId: 7
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693083881/pexels-photo-164338_ksdu9n.jpg",
+      preview: true,
+      spotId: 8
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084183/pexels-photo-2121121_s1oh3m.jpg",
+      preview: false,
+      spotId: 9
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-2635038_wj3xvr.webp",
+      preview: false,
+      spotId: 8
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-2724749_r7i2ng.webp",
+      preview: false,
+      spotId: 9
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084183/pexels-photo-1080696_zaryd5.jpg",
+      preview: false,
+      spotId: 7
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-276724_vyzstu.jpg",
+      preview: false,
+      spotId: 8
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1692757511/samples/balloons.jpg",
+      preview: false,
+      spotId: 9
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084183/pexels-photo-1080696_zaryd5.jpg",
+      preview: false,
+      spotId: 10
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1693084182/pexels-photo-276724_vyzstu.jpg",
+      preview: false,
+      spotId: 10
+    },
+    {
+      url: "https://res.cloudinary.com/dbnngk5fd/image/upload/v1692757511/samples/balloons.jpg",
+      preview: false,
+      spotId: 10
+    }
 
   ], {})
 
